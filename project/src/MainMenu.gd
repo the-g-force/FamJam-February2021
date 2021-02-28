c8bc9e32805319c6982348f1f506368e3440ea27
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 13:21:45 -0500

    Replace button with keyboard navigation

--- project/src/MainMenu.gd
+++ project/src/MainMenu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 
+func _input(event):
+	if event is InputEventKey and event.pressed and event.scancode == KEY_ENTER:
+		var _ignored := get_tree().change_scene("res://src/GameplayLevel.tscn")
 
-func _on_Button_pressed():
-	var _ignored := get_tree().change_scene("res://src/GameplayLevel.tscn")
