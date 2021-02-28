edb3b73e569efab1aea062cd7eb5ae7562e0a2c1
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 11:39:07 -0500

    Replace Roboto with DMMono

--- project/src/FancyLetter.gd
+++ project/src/FancyLetter.gd
@@ -1,12 +1,14 @@
 extends Label
 
+const _FONT_DATA := preload("res://assets/fonts/DMMono-Regular.ttf")
+
 onready var _animation_player := $AnimationPlayer
 
 func _ready():
 	# Each letter needs its own font so that they can be independently
 	# animated (e.g. change the size of one and not the others)
 	var font = DynamicFont.new()
-	font.font_data = preload("res://assets/fonts/Roboto-Medium.ttf")
+	font.font_data = _FONT_DATA
 	add_font_override("font", font)
 
 
