91201ccbcb307db8e0bc1f279486da98eee5926b
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 11:53:29 -0500

    Improve word animation nominally

--- project/src/FancyLetter.gd
+++ project/src/FancyLetter.gd
@@ -9,8 +9,15 @@ func _ready():
 	# animated (e.g. change the size of one and not the others)
 	var font = DynamicFont.new()
 	font.font_data = _FONT_DATA
+	font.size = 32
+	font.outline_size = 1
+	font.outline_color = Color.black
 	add_font_override("font", font)
 
 
-func animate():
-	_animation_player.play("Awful")
+func play_next_letter_animation()->void:
+	_animation_player.play("next_letter")
+
+
+func play_success_animation()->void:
+	_animation_player.play_backwards("next_letter")
--- project/src/FancyWord.gd
+++ project/src/FancyWord.gd
@@ -18,13 +18,16 @@ func _generate_word():
 		fancy_letter.name = letter
 		fancy_letter.text = letter
 		add_child(fancy_letter)
+		get_child(0).call_deferred("play_next_letter_animation")
 
 
 func attempt(letter:String) -> bool:
 	letter = letter.to_upper()
 	if letter == word[_next_letter]:
-		get_child(_next_letter).animate()
+		get_child(_next_letter).play_success_animation()
 		_next_letter += 1
+		if not is_complete():
+			get_child(_next_letter).play_next_letter_animation()
 		return true
 	else:
 		return false
