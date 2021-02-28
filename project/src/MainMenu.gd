273d781c7a397518d57c6e988b64b0d3c2e2640f
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 14:39:52 -0500

    Add difficulty levels to the main menu

--- project/src/MainMenu.gd
+++ project/src/MainMenu.gd
@@ -1,5 +1,19 @@
 extends Control
 
+const DIFFICULTY_DURATIONS = {
+	0: 20,
+	1: 8,
+	2: 4,
+	3: 2
+}
+
+onready var _option_button := $DifficultyBox/OptionButton
+
+
+func _ready():
+	_option_button.theme = Theme.new()
+	_option_button.theme.default_font = preload("res://src/main_menu_text.tres")
+
 
 func _input(event):
 	if event is InputEventKey and event.pressed and event.scancode == KEY_ENTER:
@@ -14,3 +28,8 @@ func _on_FullscreenToggle_toggled(button_pressed:bool):
 func _on_MuteToggle_toggled(button_pressed:bool):
 	var index := AudioServer.get_bus_index("Music")
 	AudioServer.set_bus_mute(index, button_pressed)
+
+
+func _on_OptionButton_item_selected(index):
+	Globals.max_duration = DIFFICULTY_DURATIONS[index]
+	print(str(Globals.max_duration))
