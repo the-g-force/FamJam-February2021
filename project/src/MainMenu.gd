49a48cab39280dc951827dc4d2a07cdf0e21aa85
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 16:31:15 -0500

    Ensure toggle buttons are in the right states upon reentry

--- project/src/MainMenu.gd
+++ project/src/MainMenu.gd
@@ -7,13 +7,21 @@ const DIFFICULTY_DURATIONS = {
 	3: 2
 }
 
-onready var _option_button := $DifficultyBox/OptionButton
+var _music_bus_index := AudioServer.get_bus_index("Music")
 
+onready var _option_button := $DifficultyBox/OptionButton
+onready var _fullscreen_toggle := $FullscreenToggle
+onready var _mute_toggle := $MuteToggle
 
 func _ready():
 	# Reset the default difficulty level.
 	Globals.max_duration = DIFFICULTY_DURATIONS[1]
 	
+	# Update the toggle buttons based on current state
+	_fullscreen_toggle.pressed = OS.window_fullscreen
+	_mute_toggle.pressed = AudioServer.is_bus_mute(_music_bus_index)
+	
+	# Set the option button theme so popups have the right font
 	_option_button.theme = Theme.new()
 	_option_button.theme.default_font = preload("res://src/main_menu_text.tres")
 
@@ -23,8 +31,7 @@ func _on_FullscreenToggle_toggled(button_pressed:bool):
 
 
 func _on_MuteToggle_toggled(button_pressed:bool):
-	var index := AudioServer.get_bus_index("Music")
-	AudioServer.set_bus_mute(index, button_pressed)
+	AudioServer.set_bus_mute(_music_bus_index, button_pressed)
 
 
 func _on_OptionButton_item_selected(index):
