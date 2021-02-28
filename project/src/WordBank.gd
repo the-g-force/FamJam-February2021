54959cad65bbd600448d450cccadb3aa48dc1ed0
Alex <alex.gestwicki@gmail.com>
2021-02-28 14:27:28 -0500

    Add no words coming up twice in a row

--- project/src/WordBank.gd
+++ project/src/WordBank.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var _word_map := {}
+var _last_word := ""
 
 func _init():
 	var count := 0
@@ -34,4 +35,12 @@ func get_random_word(length:int)->String:
 	var words = _word_map[length]
 	if words==null:
 		push_error("Requested length %d but it does not exist" % length)
-	return words[randi() % words.size()]
+	var finding_word := true
+	var word:String = words[randi() % words.size()]
+	while finding_word:
+		if word != _last_word:
+			finding_word = false
+			_last_word = word
+		else:
+			word = words[randi() % words.size()]
+	return word
