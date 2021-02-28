094244994507f01ae8126bd429a9a7d51a293ebe
Alex <alex.gestwicki@gmail.com>
2021-02-28 11:39:15 -0500

    Add aliens go from left to right

--- project/src/GameOverScene.gd
+++ project/src/GameOverScene.gd
@@ -1,5 +1,11 @@
 extends Control
 
+var target := "rover"
+
+
+func _ready():
+	$Label.text = "Oh no! The aliens got your "+target+"!"
+
 
 func _on_Button_pressed():
 	var _ignored := get_tree().change_scene("res://src/GameplayLevel.tscn")
