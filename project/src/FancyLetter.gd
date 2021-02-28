7fa2adbe17c69d120c7dffc00d67c671523e6758
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 10:35:05 -0500

    Implement FancyWord attempt and is_complete

--- project/src/FancyLetter.gd
+++ project/src/FancyLetter.gd
@@ -1,1 +1,12 @@
 extends Label
+
+
+func _ready():
+	# Each letter needs its own font so that they can be independently
+	# animated (e.g. change the size of one and not the others)
+	var font = DynamicFont.new()
+	font.font_data = preload("res://assets/fonts/Roboto-Medium.ttf")
+	add_font_override("font", font)
+
+func animate():
+	$AnimationPlayer.play("Awful")
--- project/src/FancyWord.gd
+++ project/src/FancyWord.gd
@@ -4,6 +4,7 @@ const _Fancy_Letter := preload("res://src/FancyLetter.tscn")
 
 var word:String = "DOggo"
 
+var _next_letter := 0
 
 func _ready():
 	word = word.to_upper()
@@ -19,9 +20,15 @@ func _generate_word():
 		add_child(fancy_letter)
 
 
-func attempt(letter) -> bool:
-	return false
+func attempt(letter:String) -> bool:
+	letter = letter.to_upper()
+	if letter == word[_next_letter]:
+		get_child(_next_letter).animate()
+		_next_letter += 1
+		return true
+	else:
+		return false
 
 
 func is_complete() -> bool:
-	return false
+	return _next_letter == word.length()
