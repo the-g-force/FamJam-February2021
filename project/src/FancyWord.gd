5c1ea7e0b5c0442a4ebc3d4ad4fbaee7a925a602
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 15:38:54 -0500

    Integrate sound for incorrect letter

--- project/src/FancyWord.gd
+++ project/src/FancyWord.gd
@@ -1,15 +1,19 @@
-extends HBoxContainer
+extends Control
 
 const _Fancy_Letter := preload("res://src/FancyLetter.tscn")
 
 var word:String setget _set_word
-
 var _next_letter := 0
+
+onready var _letter_box := $LetterBox
+
+
+func _ready():
+	_generate_word()
 
 
 func _set_word(value):
 	word = value.to_upper()
-	_generate_word()
 
 
 func _generate_word():
@@ -17,21 +21,22 @@ func _generate_word():
 		var fancy_letter := _Fancy_Letter.instance()
 		fancy_letter.name = letter
 		fancy_letter.text = letter
-		add_child(fancy_letter)
-		get_child(0).call_deferred("play_next_letter_animation")
+		_letter_box.add_child(fancy_letter)
+		_letter_box.get_child(0).call_deferred("play_next_letter_animation")
 
 
 func attempt(letter:String) -> bool:
 	letter = letter.to_upper()
 	if letter == word[_next_letter]:
-		get_child(_next_letter).play_success_animation()
-		get_child(_next_letter).modulate = Color.white
+		_letter_box.get_child(_next_letter).play_success_animation()
+		_letter_box.get_child(_next_letter).modulate = Color.white
 		_next_letter += 1
 		if not is_complete():
-			get_child(_next_letter).play_next_letter_animation()
+			_letter_box.get_child(_next_letter).play_next_letter_animation()
 		return true
 	else:
-		get_child(_next_letter).play_mistake_animation()
+		_letter_box.get_child(_next_letter).play_mistake_animation()
+		$MistakeSound.play()
 		return false
 
 
