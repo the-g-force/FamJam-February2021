5831f3649a12839cbff3fa7135660c1b04ad070b
Paul Gestwicki <pvgestwicki@bsu.edu>
2021-02-28 13:10:08 -0500

    Start with a countdown timer

--- project/src/GameplayLevel.gd
+++ project/src/GameplayLevel.gd
@@ -8,11 +8,17 @@ const _ALIENS := [
 const _FancyWord := preload("res://src/FancyWord.tscn")
 const _Explosion := preload("res://src/Explosion.tscn")
 
+enum State {
+	COUNTING_DOWN,
+	PLAYING,
+	GAME_OVER
+}
+
 var _word_bank = preload("res://src/WordBank.gd").new()
-var _game_over := false
+var _state = State.COUNTING_DOWN
 var _time_elapsed := 0.0
 
-export var _total_time := 6.0
+export var _total_time := 8.0
 
 onready var _word := $WordBox/FancyWord
 onready var _word_box := $WordBox
@@ -21,15 +27,11 @@ onready var _start_pos:Vector2 = $StartPos.get_global_transform().origin
 onready var _end_pos:Vector2 = $EndPos.get_global_transform().origin
 onready var _game_over_control := $GameOver
 onready var _target := $Rover
-
-
-func _ready():
-	_generate_word()
-	_new_alien()
+onready var _countdown_timer := $CountdownTimer
 
 
 func _process(delta):
-	if not _game_over:
+	if _state == State.PLAYING:
 		_time_elapsed += delta/_total_time
 		var pos = lerp(_start_pos, _end_pos, _time_elapsed)
 		_alien_slot.position = pos
@@ -38,7 +40,7 @@ func _process(delta):
 
 
 func game_over()->void:
-	_game_over = true
+	_state = State.GAME_OVER
 	var _explosion := _Explosion.instance()
 	_explosion.position = _target.get_global_transform().origin
 	add_child(_explosion)
@@ -53,7 +55,10 @@ func _generate_word()->void:
 
 
 func _input(event):
-	if event is InputEventKey and event.is_pressed() and not event.is_echo() and not _game_over:
+	if _state != State.PLAYING:
+		return
+		
+	if event is InputEventKey and event.is_pressed() and not event.is_echo():
 		var the_char := char(event.unicode)
 		var success : bool = _word.attempt(the_char)
 		match success:
@@ -61,7 +66,7 @@ func _input(event):
 				var complete:bool = _word.is_complete()
 				if complete:
 					_remove_alien_sprite()
-					_new_alien()
+					_create_new_alien()
 					_word_box.remove_child(_word)
 					_generate_word()
 			false:
@@ -72,7 +77,14 @@ func _remove_alien_sprite()->void:
 	_alien_slot.get_child(0).queue_free()
 
 
-func _new_alien()->void:
+func _create_new_alien()->void:
 	_time_elapsed = 0.0
 	var alien:AnimatedSprite = _ALIENS[randi()%_ALIENS.size()].instance()
 	_alien_slot.add_child(alien)
+
+
+func _on_CountdownTimer_done():
+	_countdown_timer.queue_free()
+	_generate_word()
+	_create_new_alien()
+	_state = State.PLAYING
